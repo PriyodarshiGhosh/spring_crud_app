34277b11572bb6f01fdad7af2844e6b57d3b8edc
Priyodarshi Ghosh <96041428+PriyodarshiGhosh@users.noreply.github.com>
2024-01-31 19:33:31 +0530

    Add files via upload

---
+++ crud/src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private int quantity;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+
+}
---
+++ crud/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	public Product saveProduct(Product product) {
+		return repository.save(product);
+	}
+	public List<Product> saveProducts(List<Product>products){
+		return repository.saveAll(products);
+	}
+	public List<Product>getProducts(){
+		return repository.findAll();
+	}
+    public Product getProductById(int id) {
+    	return repository.findById(id).orElse(null);
+    }
+    public Product getProductByName(String name) {
+    	return repository.findByName(name);
+    }
+    public String deleteProduct(int id) {
+    	repository.deleteById(id);
+    	return "deleted";
+    }
+    public Product updateProduct(Product product) {
+    	Product existingProduct=repository.findById(product.getId()).orElse(null);
+    	existingProduct.setName(product.getName());
+    	existingProduct.setQuantity(product.getQuantity());
+    	existingProduct.setPrice(product.getPrice());
+    	return repository.save(existingProduct);
+    }
+    
+}
